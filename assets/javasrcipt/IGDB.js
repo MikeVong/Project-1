aeeda0a7c4ef896c8160d5564e59f606becc9adc
Hunter Lintz <hunterlintz126@gmail.com>
2020-03-21 14:33:57 -0400

    Added Cover Image
    Added Cover Image

--- assets/javasrcipt/IGDB.js
+++ assets/javasrcipt/IGDB.js
@@ -1,13 +1,28 @@
-var gameInfoSearch = "minecraft";
-var infoQueryURL = "https://api.rawg.io/api/games?search="+ gameSearch;
 
+$("#search-button").on("click", function(event) 
+    {
+    event.preventDefault();
+    if ($("#search-game").val().trim() === "")
+    {
+        alert("Please enter a game");
+    }
+    else
+    {
+        getGame();
+    }
+    
+		});
+		
 function getGame(){
+	var gameInfoSearch = $("#search-game").val().trim();
+	var infoQueryURL = "https://api.rawg.io/api/games?search="+ gameInfoSearch;
+	$("#cover-goes-here").empty();
 	$.ajax({
 			url:infoQueryURL,
 			method:"GET",
-		}).then(function(response)
+		}).then(function(infoResponse)
 			{
-			console.log(response);
+			console.log(infoResponse);
+			$("#cover-goes-here").html("<img class='card' style='width:300px' src="+ infoResponse.results[0].background_image + ">");
 			});
 };
-getGame();
