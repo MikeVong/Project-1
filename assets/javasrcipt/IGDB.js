62bc4542b4bae2f74a064a5e9f22e7374042cb00
Mike Vong <mv910@hotmail.com>
2020-03-23 16:04:19 -0700

    added platform logos

--- assets/javasrcipt/IGDB.js
+++ assets/javasrcipt/IGDB.js
@@ -27,5 +27,14 @@ function getGame(){
 			$("#game-description").html("<h2>" + infoResponse.results[0].name + "</h2>");
 			$("#release-date").html("<h4>" + infoResponse.results[0].released + "</h4>");
 			$("#genre").html("<h4>" + infoResponse.results[0].genres[0].name + "</h4>");
+			var platform1 = infoResponse.results[0].platforms[0].platform.name;
+			
+			
+			$("#platform1").html("<img id= 'pix' src='./assets/images/pc.png'>");
+			$("#platform2").html("<img id= 'pix' src='./assets/images/ps4.jpg'>");
+			$("#platform3").html("<img id= 'pix' src='./assets/images/xbox.jpg'>");
+			$("#platform4").html("<img id= 'pix' src='./assets/images/ios.png'>");
+
+			
 			});
 };
