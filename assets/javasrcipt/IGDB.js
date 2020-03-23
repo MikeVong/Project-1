6b2f8bb36c758ef545d4f1938eafface4987f538
Mike Vong <mv910@hotmail.com>
2020-03-23 16:41:02 -0700

    added for loop for platforms

--- assets/javasrcipt/IGDB.js
+++ assets/javasrcipt/IGDB.js
@@ -27,13 +27,77 @@ function getGame(){
 			$("#game-description").html("<h2>" + infoResponse.results[0].name + "</h2>");
 			$("#release-date").html("<h4>" + infoResponse.results[0].released + "</h4>");
 			$("#genre").html("<h4>" + infoResponse.results[0].genres[0].name + "</h4>");
-			var platform1 = infoResponse.results[0].platforms[0].platform.name;
 			
-			
-			$("#platform1").html("<img id= 'pix' src='./assets/images/pc.png'>");
-			$("#platform2").html("<img id= 'pix' src='./assets/images/ps4.jpg'>");
-			$("#platform3").html("<img id= 'pix' src='./assets/images/xbox.jpg'>");
-			$("#platform4").html("<img id= 'pix' src='./assets/images/ios.png'>");
+			$("#platform").empty();
+			for (var i=0;i<4;i++)
+			{
+				
+				var pName =infoResponse.results[0].platforms[i].platform.name;
+				if(pName ==="PC")
+				{
+				var newCol =$("<div>");
+				newCol.addClass("col-sm-3");
+				var pImg = $("<img>");
+				pImg.attr("width", "100");
+				pImg.attr("height", "100");
+				pImg.addClass("data-image");
+				pImg.attr("src","./assets/images/pc.png");
+				newCol.append(pImg);
+				$("#platform").append(newCol);
+				}
+				else if (pName === "PlayStation 4")
+				{
+				var newCol =$("<div>");
+				newCol.addClass("col-sm-3");
+				var pImg = $("<img>");
+				pImg.attr("width", "100");
+				pImg.attr("height", "100");
+				pImg.addClass("data-image");
+				pImg.attr("src","./assets/images/ps4.jpg");
+				newCol.append(pImg);
+				$("#platform").append(newCol);
+				}
+				else if (pName === "Xbox One")
+				{
+				var newCol =$("<div>");
+				newCol.addClass("col-sm-3");
+				var pImg = $("<img>");
+				pImg.attr("width", "100");
+				pImg.attr("height", "100");
+				pImg.addClass("data-image");
+				pImg.attr("src","./assets/images/xbox.jpg");
+				newCol.append(pImg);
+				$("#platform").append(newCol);
+				}
+				else if (pName === "iOS")
+				{
+				var newCol =$("<div>");
+				newCol.addClass("col-sm-3");
+				var pImg = $("<img>");
+				pImg.attr("width", "100");
+				pImg.attr("height", "100");
+				pImg.addClass("data-image");
+				pImg.attr("src","./assets/images/ios.png");
+				newCol.append(pImg);
+				$("#platform").append(newCol);
+				}
+
+				/*
+				var newCol =$("<div>");
+				newCol.addClass("col-sm-3");
+				var pImg = $("<img>");
+				pImg.attr("width", "100");
+				pImg.attr("height", "100");
+				pImg.addClass("data-image");
+				pImg.attr("src","./assets/images/pc.png");
+				newCol.append(pImg);
+				$("#platform").append(newCol);
+				*/
+			};
+			//$("#platform1").html("<img id= 'pix' src='./assets/images/pc.png'>");
+			//$("#platform2").html("<img id= 'pix' src='./assets/images/ps4.jpg'>");
+			//$("#platform3").html("<img id= 'pix' src='./assets/images/xbox.jpg'>");
+			//$("#platform4").html("<img id= 'pix' src='./assets/images/ios.png'>");
 
 			
 			});
