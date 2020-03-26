ad009b7d20db74e86ef33ab4c70fc2dec74999c2
Hunter Lintz <hunterlintz126@gmail.com>
2020-03-25 23:26:06 -0400

    Fixed Up Code and Added Comments
    Fixed Up Code and Added Comments

--- assets/javasrcipt/recent.js
+++ assets/javasrcipt/recent.js
@@ -1,49 +1,35 @@
 import {gameName} from './RAWG.js';
 
 // Your web app's Firebase configuration
-var config = 
-    {
+var config ={
     apiKey: "AIzaSyBBiv6rFKaBEsA2VMz2PadDyVz2PKzASXI",
     authDomain: "project-1-70719.firebaseapp.com",
     databaseURL: "https://project-1-70719.firebaseio.com",
     storageBucket: "project-1-70719.appspot.com"
-    };
+};
 // Initialize Firebase
 firebase.initializeApp(config);
-
-
 var database = firebase.database();
+$("#search-button").on("click", function(event){
+	event.preventDefault();
+	var rSearch= gameName;
+	var newRecent={
+			rSearch: rSearch,
+			dateAdded: firebase.database.ServerValue.TIMESTAMP
+	};
+	database.ref().push(newRecent);
+	$(".recentItem:first").addClass("animated hinge");
 
-$("#search-button").on("click", function(event) 
-    {
-    event.preventDefault();
-    var rSearch= gameName;
-    
-    var newRecent=
-        {
-        rSearch: rSearch,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
-        };
-
-    database.ref().push(newRecent);
-    $(".recentItem:first").addClass("animated hinge");
-    setTimeout(function() {
-        $(".recentItem:first").remove();
-    }, 1000);
-    
-
-    });
-
-    database.ref().orderByChild("dateAdded").limitToLast(5).on("child_added", function(childSnapshot) 
-    {
-        
-        var recentDisplay = childSnapshot.val().rSearch;
-        var newRow = $("<div>");
-        newRow.addClass("card recentItem animated fadeIn");
-        newRow.append(recentDisplay);
-        $("#recentTable").append(newRow);
-
-        $("#search-game").val("");
-
-        
-    });
+	setTimeout(function() {
+			$(".recentItem:first").remove();
+	}, 1000);
+});
+//show the last 5 items added to the database
+database.ref().orderByChild("dateAdded").limitToLast(5).on("child_added", function(childSnapshot){	
+	var recentDisplay = childSnapshot.val().rSearch;
+	var newRow = $("<div>");
+	newRow.addClass("card recentItem animated fadeIn");
+	newRow.append(recentDisplay);
+	$("#recentTable").append(newRow);
+	$("#search-game").val("");
+});
--- assets/javasrcipt/youtubeApp.js
+++ assets/javasrcipt/youtubeApp.js
@@ -3,67 +3,53 @@ var apiKey = "AIzaSyAVtUWiVEDWPVgD6HebwsUXHJ3PP6qj9gE";
 // api url website link
 var queryURL ="https://www.googleapis.com/youtube/v3/search";
 
-
-
-$("#search-button").on("click", function(event) 
-    {
+var gameSearch;
+//checks if search bar isnt empty then runs the video search
+$("#search-button").on("click", function(event){
     event.preventDefault();
-    if ($("#search-game").val().trim() === "")
-    {
+    if ($("#search-game").val().trim() === ""){
         $('#\\#myModal').modal('show');
-    }
-    else
-    {
+    }else{
+				var gameSearch = $("#search-game").val().trim();
         getVideo();
-
     }
-    
-    });
-
-    function getVideo()
-    {
-    var gameSearch = $("#search-game").val().trim();
-    gameSearch = gameSearch + " review";
-    var options =
-        {
-        key: apiKey,
-        q: gameSearch,
-        part: 'snippet',
-        maxResults: 3,
-        type: 'video',
-        videoEmbeddable: true,
-        };
-    
-    $.ajax(
-        {
-          url: queryURL,
-          method: "GET",
-          data: options
-        })
-        .then(function(response) 
-                {
-                console.log(response);
-                console.log(response.items[0].id.videoId);
-                $("#youtubeHere").empty();
-                for (var i = 0; i < response.items.length; i++)
-                        {
-                        var videoLink = response.items[i].id.videoId;
-                        var newRow=$("<div>");
-                        newRow.addClass("col-md-4");
-                        var img = $("<iframe>");
-                        img.attr("width","360");
-												img.attr("height", "260");
-												newRow.attr("margin-right", "20px");
-                        img.attr("src", "https://www.youtube.com/embed/"+ videoLink);
-                        img.addClass("card animated delay-5s bounceInUp");
-												newRow.append(img);
-                        $("#youtubeHere").append(newRow);
-                        }; 
-                });
-                    
-    };
-
-
-
-
-
+});
+//function that gets video and places them on screen
+function getVideo(){
+	gameSearch = gameSearch + " review";
+	var options ={
+			key: apiKey,
+			q: gameSearch,
+			part: 'snippet',
+			maxResults: 3,
+			type: 'video',
+			videoEmbeddable: true,
+	};
+	$.ajax({
+				url: queryURL,
+				method: "GET",
+				data: options
+	}).then(function(response){
+		$("#youtubeHere").empty();
+		for (var i = 0; i < response.items.length; i++){
+						var videoLink = response.items[i].id.videoId;
+						var newRow=$("<div>");
+						newRow.addClass("col-md-4");
+						var img = $("<iframe>");
+						img.attr("width","360");
+						img.attr("height", "260");
+						newRow.attr("margin-right", "20px");
+						img.attr("src", "https://www.youtube.com/embed/"+ videoLink);
+						img.addClass("card animated delay-5s bounceInUp");
+						newRow.append(img);
+						$("#youtubeHere").append(newRow);
+		}; 
+	});                
+};
+//Function that runs when you click a recent item
+$(document).on("click",".recentItem",function(){
+	gameSearch = $(this).text();
+	console.log(this);
+	console.log(gameSearch);
+	getVideo();
+});
